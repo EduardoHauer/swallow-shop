8754340b3b22600a0da1dbc7ed8ae6f55da5f07f
Eduardo Lovera Hauer <eloverahauer@gmail.com>
2024-10-01 17:16:58 -0300

    Project created

---
+++ src/main/java/com/workshop/swallowshop/SwallowshopApplication.java
@@ -0,0 +1,14 @@
+package com.workshop.swallowshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwallowshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwallowshopApplication.class, args);
+	}
+
+	
+}
---
+++ src/test/java/com/workshop/swallowshop/SwallowshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.swallowshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwallowshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
